ee022582ca6d6156120897c535b0abfc934617fb
Sentissi Mouad <m.sentissi@gmail.com>
2022-12-14 21:18:24 +0100

    catching http request/headers as well as soap request

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // Based on https://www.npmjs.com/package/soap#received-soap-headers
 var express = require('express');
+//const req = require('express/lib/request');
 var soap = require('soap');
 
 // the splitter function, used by the service
 function splitter_function(args) {
-    console.log('splitter_function1111');
+    console.log("\n ==> splitter_function invoked ...");
     var splitter = args.splitter;
     var splitted_msg = args.message.split(splitter);
     var result = [];
@@ -18,7 +19,7 @@ function splitter_function(args) {
 
 // the count function, used by the service
 function count_function(args) {
-    console.log('count_function2222');
+    console.log("\ncount_function invoked ...");
     var splitter = args.splitter;
     var splitted_msg = args.message.split(splitter);
     var result = [];
@@ -46,11 +47,24 @@ var serviceObject = {
   var app = express();
   //body parser middleware are supported (optional)
   //app.use(bodyParser.raw({type: function(){return true;}, limit: '5mb'}));
-  app.listen(8001, function(){
-      //Note: /wsdl route will be handled by soap module
-      //and all other routes & middleware will continue to work
+  
+ // var http = require('http');
+  app.listen(8001);
 
-      soapServer = soap.listen(app, '/wsdl', serviceObject, xml, function(){
-        console.log('server initialized on 8001');
-      });
+  app.post('/wsdl', (req, res, next) => {
+    console.log("\n==> Auth in HTTP headers : " + req.headers.authorization);
+    next();
+  });
+
+  soapServer = soap.listen(app, '/wsdl', serviceObject, xml, function(){
+    console.log('server initialized on 8001');
+  });
+  
+  soapServer.on('request', function(request, methodName) {
+    // It is possible to change the value of the headers
+    // before they are handed to the service method.
+    // It is also possible to throw a SOAP Fault
+    console.log("\n==> SoapAction : " + methodName);
+    console.log("\n==> SOAP request :");
+    console.log(request);
   });
